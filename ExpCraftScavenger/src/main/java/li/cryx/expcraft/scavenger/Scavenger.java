6461cc4c3dc92ac0e09a887f42136aa694016d29
cryxli <cryxli@cryx.li>
2011-11-29 15:05:06 +0100

    Added my version of the Scavenger module. Contains preparations for MC 1.0

---
+++ ExpCraftScavenger/src/main/java/li/cryx/expcraft/scavenger/Scavenger.java
@@ -0,0 +1,108 @@
+package li.cryx.expcraft.scavenger;
+
+import java.util.logging.Logger;
+
+import li.cryx.expcraft.module.ExpCraftModule;
+import li.cryx.expcraft.util.Chat;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.Event.Priority;
+import org.bukkit.event.Event.Type;
+import org.bukkit.plugin.PluginManager;
+
+public class Scavenger extends ExpCraftModule {
+	public final Logger LOG = Logger.getLogger("EC-Scavenger");
+
+	private ScavengerBlockListener blockListener;
+
+	private Chat chat;
+
+	/**
+	 * Create listeners and link them to this plugin and a common
+	 * <code>Chat</code> component.
+	 */
+	private void createListeners() {
+		// use one chat tool
+		chat = new Chat(this);
+
+		// block listener
+		blockListener = new ScavengerBlockListener(this);
+	}
+
+	@Override
+	public String getAbbr() {
+		return "Sc";
+	}
+
+	/**
+	 * Get the <code>int</code> value of the given config key.
+	 * 
+	 * @param key
+	 *            Key in the config YAML.
+	 * @return Value associated with the given key.
+	 */
+	double getConfDouble(final String key) {
+		return getConfig().getDouble(key);
+	}
+
+	/**
+	 * Get the <code>double</code> value of the given config key.
+	 * 
+	 * @param key
+	 *            Key in the config YAML.
+	 * @return Value associated with the given key.
+	 */
+	int getConfInt(final String key) {
+		return getConfig().getInt(key);
+	}
+
+	@Override
+	public String getName() {
+		return "Scavenger";
+	}
+
+	/**
+	 * Load config from disk merge missing default values and store them to
+	 * disk.
+	 */
+	private void loadConfig() {
+		// force loading config from default once
+		getConfig();
+		// ...to store at first run
+		saveConfig();
+	}
+
+	// server want the plugin to stop working
+	@Override
+	public void onDisable() {
+		// disabled plugins don't get events; no need to unregister
+		// listeners
+		LOG.info("[EC] " + getDescription().getFullName() + " disabled");
+	}
+
+	// server want the plugin to start working
+	@Override
+	public void onEnable() {
+		// pre-load config
+		loadConfig();
+		// register listeners
+		registerEvents();
+		// ready
+		LOG.info("[EC] " + getDescription().getFullName() + " enabled");
+	}
+
+	/** Register the listeners */
+	private void registerEvents() {
+		// ensure the listeners are ready
+		createListeners();
+		// register listeners
+		PluginManager pm = getServer().getPluginManager();
+		pm.registerEvent(Type.BLOCK_BREAK, blockListener, Priority.Highest,
+				this);
+	}
+
+	void sendHint(final Player player, final String msg) {
+		chat.info(player, msg);
+	}
+
+}
