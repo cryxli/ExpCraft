52669de9126ee9323cf6b625067ca94afe0e5410
cryxli <git@cryx.li>
2012-08-12 13:33:16 +0200

    Refactored scavener block listener

--- ExpCraftScavenger/src/main/java/li/cryx/expcraft/scavenger/ScavengerBlockListener.java
+++ ExpCraftScavenger/src/main/java/li/cryx/expcraft/scavenger/ScavengerBlockListener.java
@@ -2,7 +2,6 @@ package li.cryx.expcraft.scavenger;
 
 import java.util.Random;
 
-import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
@@ -10,7 +9,6 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
-import org.bukkit.inventory.ItemStack;
 
 /**
  * This class implements a block listener that traces block destruction, and,
@@ -26,182 +24,21 @@ public class ScavengerBlockListener implements Listener {
 	/** Reference to parent plugin. */
 	private final Scavenger plugin;
 
+	/** Externalised logic */
+	private final ScavengerConstraints test;
+
 	/** Create a new listener for the given plugin. */
-	public ScavengerBlockListener(final Scavenger instance) {
-		plugin = instance;
-	}
-
-	private void dropItem(final Block block, final Material material) {
-		Location locy = new Location(block.getWorld(), block.getX(),
-				block.getY(), block.getZ(), 0.0f, 0.0f);
-		block.getWorld().dropItem(locy, new ItemStack(material, 1));
-	}
-
-	/**
-	 * Calculate drops for the Nether.
-	 * 
-	 * @param player
-	 *            Current player
-	 * @param level
-	 *            Player's level
-	 * @param block
-	 *            The block that was destroyed
-	 * @param rnd
-	 *            A random number
-	 */
-	private void dropNether(final Player player, final int level,
-			final Block block, final int rnd) {
-		if (rnd <= 3 && level >= plugin.getConfInt("DropLevel.GhastTear")) {
-			// 4 %o - 4%
-			plugin.getPersistence().addExp(plugin, player,
-					plugin.getConfDouble("ExpGain.GhastTear"));
-			dropItem(block, Material.GHAST_TEAR);
-			plugin.sendHint(player, "A Ghast must have been sad, here.");
-
-		} else if (rnd > 3 && rnd <= 6
-				&& level >= plugin.getConfInt("DropLevel.Nugget")) {
-			// 3 %o - 3%
-			plugin.getPersistence().addExp(plugin, player,
-					plugin.getConfDouble("ExpGain.Nugget"));
-			dropItem(block, Material.GOLD_NUGGET);
-
-		} else if (rnd > 6 && rnd <= 8
-				&& level >= plugin.getConfInt("DropLevel.BlazePowder")) {
-			// 2 %o - 2%
-			plugin.getPersistence().addExp(plugin, player,
-					plugin.getConfDouble("ExpGain.BlazePowder"));
-			dropItem(block, Material.BLAZE_POWDER);
-
-		}
-	}
-
-	/**
-	 * Calculate drops for the normal world.
-	 * 
-	 * @param player
-	 *            Current player
-	 * @param level
-	 *            Player's level
-	 * @param block
-	 *            The block that was destroyed
-	 * @param rnd
-	 *            A random number
-	 */
-	private void dropNormal(final Player player, final int level,
-			final Block block, final int rnd) {
-		if (rnd == 0 && level >= plugin.getConfInt("DropLevel.Chestplate")) {
-			// 1 %o - 1% (old: 0)
-			plugin.getPersistence().addExp(plugin, player,
-					plugin.getConfDouble("ExpGain.Chestplate"));
-			dropItem(block, Material.LEATHER_CHESTPLATE);
-
-		} else if (rnd > 0 && rnd <= 5
-				&& level >= plugin.getConfInt("DropLevel.Saddle")) {
-			// 5 %o - 5%
-			dropItem(block, Material.SADDLE);
-			plugin.getPersistence().addExp(plugin, player,
-					plugin.getConfDouble("ExpGain.Saddle"));
-
-		} else if (rnd == 5 && level >= plugin.getConfInt("DropLevel.Ingot")) {
-			// 1 %o - 1%
-			plugin.getPersistence().addExp(plugin, player,
-					plugin.getConfDouble("ExpGain.Ingot"));
-			dropItem(block, Material.GOLD_INGOT);
-			plugin.sendHint(player,
-					"Hey, do you see that shiny thing in the sand?.");
-
-		} else if (rnd > 5 && rnd <= 7
-				&& level >= plugin.getConfInt("DropLevel.Bowl")) {
-			// 3 %O -3%
-			plugin.getPersistence().addExp(plugin, player,
-					plugin.getConfDouble("ExpGain.Bowl"));
-			dropItem(block, Material.BOWL);
-
-		} else if (rnd > 7 && rnd <= 9
-				&& level >= plugin.getConfInt("DropLevel.Boots")) {
-			// 2 %o - 2% (ori: 3 %o - 3%)
-			plugin.getPersistence().addExp(plugin, player,
-					plugin.getConfDouble("ExpGain.Boots"));
-			dropItem(block, Material.LEATHER_BOOTS);
-
-		} else if (rnd > 9 && rnd <= 11
-				&& level >= plugin.getConfInt("DropLevel.Helmet")) {
-			// 2 %o - 2% (ori: 0)
-			plugin.getPersistence().addExp(plugin, player,
-					plugin.getConfDouble("ExpGain.Helmet"));
-			dropItem(block, Material.LEATHER_HELMET);
-
-		} else if (rnd > 11 && rnd <= 14
-				&& level >= plugin.getConfInt("DropLevel.Bucket")) {
-			// 3 %o - 3% (ori: 4 %o - 4%)
-			plugin.getPersistence().addExp(plugin, player,
-					plugin.getConfDouble("ExpGain.Bucket"));
-			dropItem(block, Material.BUCKET);
-
-		} else if (rnd > 14 && rnd <= 16
-				&& level >= plugin.getConfInt("DropLevel.Leggins")) {
-			// 2 %o - 2% (ori: 0)
-			plugin.getPersistence().addExp(plugin, player,
-					plugin.getConfDouble("ExpGain.Leggins"));
-			dropItem(block, Material.LEATHER_LEGGINGS);
-
-		}
-	}
-
-	/**
-	 * Depending on the player's level, he may have a higher chance to find
-	 * items.
-	 * 
-	 * @param level
-	 *            Current player's level.
-	 * @return
-	 */
-	private int getRange(final int level) {
-		int levelCap = plugin.getLevelCap();
-		int i = 1000;
-		if (level >= 0.05 * levelCap && level < 0.1 * levelCap) {
-			i = 900;
-		} else if (level >= 0.1 * levelCap && level < 0.2 * levelCap) {
-			i = 850; // 600;
-		} else if (level >= 0.2 * levelCap && level < 0.4 * levelCap) {
-			i = 650; // 500;
-		} else if (level >= 0.4 * levelCap && level < 0.7 * levelCap) {
-			i = 400; // 300;
-		} else if (level >= 0.7 * levelCap && level < 0.9 * levelCap) {
-			i = 200; // 150;
-		} else if (level >= 0.9 * levelCap) {
-			i = 100;
-		}
-		return i;
-	}
-
-	/**
-	 * Test whether the block destroyed was material were scavenging could
-	 * happen.
-	 * 
-	 * @param material
-	 *            The destroyed block
-	 * @return <code>true</code>, if material is dirt, grass, sand, gravel or
-	 *         soulsand.
-	 */
-	private boolean isDiggable(final Material material) {
-		return material == Material.DIRT || //
-				material == Material.GRASS || //
-				material == Material.SAND || //
-				material == Material.GRAVEL || //
-				material == Material.CLAY || //
-				material == Material.SOUL_SAND;
+	public ScavengerBlockListener(final Scavenger plugin) {
+		this.plugin = plugin;
+		test = new ScavengerConstraints(plugin);
 	}
 
 	// A block has been destroyed
-	@EventHandler(priority = EventPriority.HIGHEST)
+	@EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
 	public void onBlockBreak(final BlockBreakEvent event) {
-		if (event.isCancelled()
-				|| !plugin.getPermission().worldCheck(
-						event.getBlock().getWorld())) {
+		if (!plugin.getPermission().worldCheck(event.getBlock().getWorld())) {
+			// the world is not managed by the ExpCraft
 			return;
-			// event is already consumed
-			// or, the world is not managed by the ExpCraft
 		}
 
 		Player player = event.getPlayer();
@@ -212,20 +49,20 @@ public class ScavengerBlockListener implements Listener {
 
 		Block block = event.getBlock();
 		Material m = block.getType();
-		if (!isDiggable(m)) {
+		if (!test.isDiggable(m)) {
 			// block is not one for scavenging
 			return;
 		}
 
 		int level = plugin.getPersistence().getLevel(plugin, player);
-		int random = randomGenerator.nextInt(getRange(level));
+		int random = randomGenerator.nextInt(test.getRange(level));
 
 		switch (player.getWorld().getEnvironment()) {
 		case NETHER:
-			dropNether(player, level, block, random);
+			test.dropNether(player, level, block, random);
 			break;
 		case NORMAL:
-			dropNormal(player, level, block, random);
+			test.dropNormal(player, level, block, random);
 			break;
 		case THE_END:
 		default:
---
+++ ExpCraftScavenger/src/main/java/li/cryx/expcraft/scavenger/ScavengerConstraints.java
@@ -0,0 +1,179 @@
+package li.cryx.expcraft.scavenger;
+
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+public class ScavengerConstraints {
+
+	private final Scavenger plugin;
+
+	public ScavengerConstraints(final Scavenger plugin) {
+		this.plugin = plugin;
+	}
+
+	public void dropItem(final Block block, final Material material) {
+		Location locy = new Location(block.getWorld(), block.getX(),
+				block.getY(), block.getZ(), 0.0f, 0.0f);
+		block.getWorld().dropItem(locy, new ItemStack(material, 1));
+	}
+
+	/**
+	 * Calculate drops for the Nether.
+	 * 
+	 * @param player
+	 *            Current player
+	 * @param level
+	 *            Player's level
+	 * @param block
+	 *            The block that was destroyed
+	 * @param rnd
+	 *            A random number
+	 */
+	public void dropNether(final Player player, final int level,
+			final Block block, final int rnd) {
+		if (rnd <= 3 && level >= plugin.getConfInt("DropLevel.GhastTear")) {
+			// 4 %o - 4%
+			plugin.getPersistence().addExp(plugin, player,
+					plugin.getConfDouble("ExpGain.GhastTear"));
+			dropItem(block, Material.GHAST_TEAR);
+			plugin.sendHint(player, "A Ghast must have been sad, here.");
+
+		} else if (rnd > 3 && rnd <= 6
+				&& level >= plugin.getConfInt("DropLevel.Nugget")) {
+			// 3 %o - 3%
+			plugin.getPersistence().addExp(plugin, player,
+					plugin.getConfDouble("ExpGain.Nugget"));
+			dropItem(block, Material.GOLD_NUGGET);
+
+		} else if (rnd > 6 && rnd <= 8
+				&& level >= plugin.getConfInt("DropLevel.BlazePowder")) {
+			// 2 %o - 2%
+			plugin.getPersistence().addExp(plugin, player,
+					plugin.getConfDouble("ExpGain.BlazePowder"));
+			dropItem(block, Material.BLAZE_POWDER);
+
+		}
+	}
+
+	/**
+	 * Calculate drops for the normal world.
+	 * 
+	 * @param player
+	 *            Current player
+	 * @param level
+	 *            Player's level
+	 * @param block
+	 *            The block that was destroyed
+	 * @param rnd
+	 *            A random number
+	 */
+	public void dropNormal(final Player player, final int level,
+			final Block block, final int rnd) {
+		if (rnd == 0 && level >= plugin.getConfInt("DropLevel.Chestplate")) {
+			// 1 %o - 1% (old: 0)
+			plugin.getPersistence().addExp(plugin, player,
+					plugin.getConfDouble("ExpGain.Chestplate"));
+			dropItem(block, Material.LEATHER_CHESTPLATE);
+
+		} else if (rnd > 0 && rnd <= 5
+				&& level >= plugin.getConfInt("DropLevel.Saddle")) {
+			// 5 %o - 5%
+			dropItem(block, Material.SADDLE);
+			plugin.getPersistence().addExp(plugin, player,
+					plugin.getConfDouble("ExpGain.Saddle"));
+
+		} else if (rnd == 5 && level >= plugin.getConfInt("DropLevel.Ingot")) {
+			// 1 %o - 1%
+			plugin.getPersistence().addExp(plugin, player,
+					plugin.getConfDouble("ExpGain.Ingot"));
+			dropItem(block, Material.GOLD_INGOT);
+			plugin.sendHint(player,
+					"Hey, do you see that shiny thing in the sand?.");
+
+		} else if (rnd > 5 && rnd <= 7
+				&& level >= plugin.getConfInt("DropLevel.Bowl")) {
+			// 3 %O -3%
+			plugin.getPersistence().addExp(plugin, player,
+					plugin.getConfDouble("ExpGain.Bowl"));
+			dropItem(block, Material.BOWL);
+
+		} else if (rnd > 7 && rnd <= 9
+				&& level >= plugin.getConfInt("DropLevel.Boots")) {
+			// 2 %o - 2% (ori: 3 %o - 3%)
+			plugin.getPersistence().addExp(plugin, player,
+					plugin.getConfDouble("ExpGain.Boots"));
+			dropItem(block, Material.LEATHER_BOOTS);
+
+		} else if (rnd > 9 && rnd <= 11
+				&& level >= plugin.getConfInt("DropLevel.Helmet")) {
+			// 2 %o - 2% (ori: 0)
+			plugin.getPersistence().addExp(plugin, player,
+					plugin.getConfDouble("ExpGain.Helmet"));
+			dropItem(block, Material.LEATHER_HELMET);
+
+		} else if (rnd > 11 && rnd <= 14
+				&& level >= plugin.getConfInt("DropLevel.Bucket")) {
+			// 3 %o - 3% (ori: 4 %o - 4%)
+			plugin.getPersistence().addExp(plugin, player,
+					plugin.getConfDouble("ExpGain.Bucket"));
+			dropItem(block, Material.BUCKET);
+
+		} else if (rnd > 14 && rnd <= 16
+				&& level >= plugin.getConfInt("DropLevel.Leggins")) {
+			// 2 %o - 2% (ori: 0)
+			plugin.getPersistence().addExp(plugin, player,
+					plugin.getConfDouble("ExpGain.Leggins"));
+			dropItem(block, Material.LEATHER_LEGGINGS);
+
+		}
+	}
+
+	/**
+	 * Depending on the player's level, he may have a higher chance to find
+	 * items.
+	 * 
+	 * @param level
+	 *            Current player's level.
+	 * @return
+	 */
+	public int getRange(final int level) {
+		int levelCap = plugin.getLevelCap();
+		int i = 1000;
+		if (level >= 0.05 * levelCap && level < 0.1 * levelCap) {
+			i = 900;
+		} else if (level >= 0.1 * levelCap && level < 0.2 * levelCap) {
+			i = 850; // 600;
+		} else if (level >= 0.2 * levelCap && level < 0.4 * levelCap) {
+			i = 650; // 500;
+		} else if (level >= 0.4 * levelCap && level < 0.7 * levelCap) {
+			i = 400; // 300;
+		} else if (level >= 0.7 * levelCap && level < 0.9 * levelCap) {
+			i = 200; // 150;
+		} else if (level >= 0.9 * levelCap) {
+			i = 100;
+		}
+		return i;
+	}
+
+	/**
+	 * Test whether the block destroyed was material were scavenging could
+	 * happen.
+	 * 
+	 * @param material
+	 *            The destroyed block
+	 * @return <code>true</code>, if material is dirt, grass, sand, gravel or
+	 *         soulsand.
+	 */
+	public boolean isDiggable(final Material material) {
+		return material == Material.DIRT || //
+				material == Material.GRASS || //
+				material == Material.SAND || //
+				material == Material.GRAVEL || //
+				material == Material.CLAY || //
+				material == Material.SOUL_SAND;
+	}
+
+}
