6f908f0ae975fc458aa0d6de5e55f91696efcb6d
cryxli <git@cryx.li>
2012-11-25 00:02:01 +0100

    Completed documentation of digging module.

---
+++ ExpCraftDigging/src/main/java/li/cryx/expcraft/digging/DiggingConst.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright (c) 2011 Urs P. Stettler, https://github.com/cryxli
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package li.cryx.expcraft.digging;
+
+/**
+ * This interface contains keys used in the modules config file.
+ * 
+ * @author cryxli
+ */
+public class DiggingConst {
+
+	/** How many experience is gained when digging up the indicated block. */
+	public static final class EXP {
+		public static final String DIRT = "ExpGain.Dirt";
+		public static final String GRASS = "ExpGain.Grass";
+		public static final String SAND = "ExpGain.Sand";
+		public static final String CLAY = "ExpGain.Clay";
+		public static final String SOULSAND = "ExpGain.SoulSand";
+		public static final String GRAVEL = "ExpGain.Gravel";
+		public static final String SNOW = "ExpGain.Snow";
+		public static final String MYCELIUM = "ExpGain.Mycelium";
+	}
+
+	/** What level in digging is required to dig up the indicated block. */
+	public static final class LEVEL {
+		public static final String DIRT = "UseLevel.Dirt";
+		public static final String GRASS = "UseLevel.Grass";
+		public static final String SAND = "ExpGain.Sand";
+		public static final String CLAY = "ExpGain.Clay";
+		public static final String SOULSAND = "ExpGain.SoulSand";
+		public static final String GRAVEL = "ExpGain.Gravel";
+		public static final String SNOW = "ExpGain.Snow";
+		public static final String MYCELIUM = "ExpGain.Mycelium";
+	}
+
+	/** Which level in digging is required to use the indicated shovel. */
+	public static final class SHOVEL {
+		/** Level for wooden shovel. */
+		public static final String WOODEN = "ShovelLevel.Wooden";
+		/** Level for (cobble)stone shovel. */
+		public static final String STONE = "ShovelLevel.Stone";
+		/** Level for iron shovel. */
+		public static final String IRON = "ShovelLevel.Iron";
+		/** Level for golden shovel. */
+		public static final String GOLD = "ShovelLevel.Gold";
+		/** Level for diamond shovel. */
+		public static final String DIAMOND = "ShovelLevel.Diamond";
+	}
+
+	public static final String FIRE_SHOVEL_LEVEL = "Settings.FireShovelLevel";
+}
--- ExpCraftDigging/src/main/java/li/cryx/expcraft/digging/DiggingContraints.java
+++ ExpCraftDigging/src/main/java/li/cryx/expcraft/digging/DiggingContraints.java
@@ -1,3 +1,25 @@
+/*
+ * Copyright (c) 2011 Urs P. Stettler, https://github.com/cryxli
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
 package li.cryx.expcraft.digging;
 
 import org.bukkit.Material;
@@ -7,84 +29,103 @@ import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.inventory.ItemStack;
 
 /**
- * TODO documentation
+ * This class contains decision logic used by the digging module.
  * 
  * @author cryxli
  */
 public class DiggingContraints {
 
+	/** Reference to digging module. */
 	private final Digging plugin;
 
 	public DiggingContraints(final Digging plugin) {
 		this.plugin = plugin;
 	}
 
-	public void addExperience(final Player player, final Material m,
+	/**
+	 * Add experience for digging related blocks to player's account.
+	 * 
+	 * @param player
+	 *            The player
+	 * @param material
+	 *            The block
+	 * @param level
+	 *            Player's level in digging
+	 */
+	public void addExperience(final Player player, final Material material,
 			final int level) {
 		double exp = 0;
-		switch (m) {
+		switch (material) {
 		case DIRT:
-			if (level < plugin.getConfig().getInteger("UseLevel.Dirt")) {
+			if (level < plugin.getConfig().getInteger(DiggingConst.LEVEL.DIRT)) {
 				plugin.warnCutBlockLevel(player,
-						plugin.getConfig().getInteger("UseLevel.Dirt"));
+						plugin.getConfig().getInteger(DiggingConst.LEVEL.DIRT));
 			} else {
-				exp = plugin.getConfig().getDouble("ExpGain.Dirt");
+				exp = plugin.getConfig().getDouble(DiggingConst.EXP.DIRT);
 			}
 			break;
 		case GRASS:
-			if (level < plugin.getConfig().getInteger("UseLevel.Grass")) {
+			if (level < plugin.getConfig().getInteger(DiggingConst.LEVEL.GRASS)) {
 				plugin.warnCutBlockLevel(player,
-						plugin.getConfig().getInteger("UseLevel.Grass"));
+						plugin.getConfig().getInteger(DiggingConst.LEVEL.GRASS));
 			} else {
-				exp = plugin.getConfig().getDouble("ExpGain.Grass");
+				exp = plugin.getConfig().getDouble(DiggingConst.EXP.GRASS);
 			}
 			break;
 		case SAND:
-			if (level < plugin.getConfig().getInteger("UseLevel.Sand")) {
+			if (level < plugin.getConfig().getInteger(DiggingConst.LEVEL.SAND)) {
 				plugin.warnCutBlockLevel(player,
-						plugin.getConfig().getInteger("UseLevel.Sand"));
+						plugin.getConfig().getInteger(DiggingConst.LEVEL.SAND));
 			} else {
-				exp = plugin.getConfig().getDouble("ExpGain.Sand");
+				exp = plugin.getConfig().getDouble(DiggingConst.EXP.SAND);
 			}
 			break;
 		case CLAY:
-			if (level < plugin.getConfig().getInteger("UseLevel.Clay")) {
+			if (level < plugin.getConfig().getInteger(DiggingConst.LEVEL.CLAY)) {
 				plugin.warnCutBlockLevel(player,
-						plugin.getConfig().getInteger("UseLevel.Clay"));
+						plugin.getConfig().getInteger(DiggingConst.LEVEL.CLAY));
 			} else {
-				exp = plugin.getConfig().getDouble("ExpGain.Clay");
+				exp = plugin.getConfig().getDouble(DiggingConst.EXP.CLAY);
 			}
 			break;
 		case SOUL_SAND:
-			if (level < plugin.getConfig().getInteger("UseLevel.SoulSand")) {
-				plugin.warnCutBlockLevel(player,
-						plugin.getConfig().getInteger("UseLevel.SoulSand"));
+			if (level < plugin.getConfig().getInteger(
+					DiggingConst.LEVEL.SOULSAND)) {
+				plugin.warnCutBlockLevel(
+						player,
+						plugin.getConfig().getInteger(
+								DiggingConst.LEVEL.SOULSAND));
 			} else {
-				exp = plugin.getConfig().getDouble("ExpGain.SoulSand");
+				exp = plugin.getConfig().getDouble(DiggingConst.EXP.SOULSAND);
 			}
 			break;
 		case GRAVEL:
-			if (level < plugin.getConfig().getInteger("UseLevel.Gravel")) {
+			if (level < plugin.getConfig()
+					.getInteger(DiggingConst.LEVEL.GRAVEL)) {
 				plugin.warnCutBlockLevel(player,
-						plugin.getConfig().getInteger("UseLevel.Gravel"));
+						plugin.getConfig()
+								.getInteger(DiggingConst.LEVEL.GRAVEL));
 			} else {
-				exp = plugin.getConfig().getDouble("ExpGain.Gravel");
+				exp = plugin.getConfig().getDouble(DiggingConst.EXP.GRAVEL);
 			}
 			break;
 		case SNOW:
-			if (level < plugin.getConfig().getInteger("UseLevel.Snow")) {
+			if (level < plugin.getConfig().getInteger(DiggingConst.LEVEL.SNOW)) {
 				plugin.warnCutBlockLevel(player,
-						plugin.getConfig().getInteger("UseLevel.Snow"));
+						plugin.getConfig().getInteger(DiggingConst.LEVEL.SNOW));
 			} else {
-				exp = plugin.getConfig().getDouble("ExpGain.Snow");
+				exp = plugin.getConfig().getDouble(DiggingConst.EXP.SNOW);
 			}
 			break;
 		case MYCEL:
-			if (level < plugin.getConfig().getInteger("UseLevel.Mycelium")) {
-				plugin.warnCutBlockLevel(player,
-						plugin.getConfig().getInteger("UseLevel.Mycelium"));
+			if (level < plugin.getConfig().getInteger(
+					DiggingConst.LEVEL.MYCELIUM)) {
+				plugin.warnCutBlockLevel(
+						player,
+						plugin.getConfig().getInteger(
+								DiggingConst.LEVEL.MYCELIUM));
 			} else {
-				exp = plugin.getConfig().getDouble("ExpGain.Mycelium");
+				exp = plugin.getConfig().getDouble(DiggingConst.EXP.MYCELIUM);
 			}
 			break;
 		default:
@@ -94,33 +135,50 @@ public class DiggingContraints {
 		plugin.getPersistence().addExp(plugin, player, exp);
 	}
 
+	/**
+	 * Check whether given player can use the item in her hand.
+	 * 
+	 * @param player
+	 *            The player
+	 * @param itemInHand
+	 *            The item in her hand
+	 * @param level
+	 *            Player's level in digging.
+	 * @return <code>true</code>, if the item is not a shovel, or the player can
+	 *         use the shovel.
+	 */
 	public boolean checkTools(final Player player, final Material itemInHand,
 			final int level) {
 
 		if (itemInHand == Material.WOOD_SPADE
-				&& level < plugin.getConfig().getInteger("ShovelLevel.Wooden")) {
+				&& level < plugin.getConfig().getInteger(
+						DiggingConst.SHOVEL.WOODEN)) {
 			plugin.warnToolLevel(player,
-					plugin.getConfig().getInteger("ShovelLevel.Wooden"));
+					plugin.getConfig().getInteger(DiggingConst.SHOVEL.WOODEN));
 
 		} else if (itemInHand == Material.STONE_SPADE
-				&& level < plugin.getConfig().getInteger("ShovelLevel.Stone")) {
+				&& level < plugin.getConfig().getInteger(
+						DiggingConst.SHOVEL.STONE)) {
 			plugin.warnToolLevel(player,
-					plugin.getConfig().getInteger("ShovelLevel.Stone"));
+					plugin.getConfig().getInteger(DiggingConst.SHOVEL.STONE));
 
 		} else if (itemInHand == Material.IRON_SPADE
-				&& level < plugin.getConfig().getInteger("ShovelLevel.Iron")) {
+				&& level < plugin.getConfig().getInteger(
+						DiggingConst.SHOVEL.IRON)) {
 			plugin.warnToolLevel(player,
-					plugin.getConfig().getInteger("ShovelLevel.Iron"));
+					plugin.getConfig().getInteger(DiggingConst.SHOVEL.IRON));
 
 		} else if (itemInHand == Material.GOLD_SPADE
-				&& level < plugin.getConfig().getInteger("ShovelLevel.Gold")) {
+				&& level < plugin.getConfig().getInteger(
+						DiggingConst.SHOVEL.GOLD)) {
 			plugin.warnToolLevel(player,
-					plugin.getConfig().getInteger("ShovelLevel.Gold"));
+					plugin.getConfig().getInteger(DiggingConst.SHOVEL.GOLD));
 
 		} else if (itemInHand == Material.DIAMOND_SPADE
-				&& level < plugin.getConfig().getInteger("ShovelLevel.Diamond")) {
+				&& level < plugin.getConfig().getInteger(
+						DiggingConst.SHOVEL.DIAMOND)) {
 			plugin.warnToolLevel(player,
-					plugin.getConfig().getInteger("ShovelLevel.Diamond"));
+					plugin.getConfig().getInteger(DiggingConst.SHOVEL.DIAMOND));
 
 		} else {
 			return true;
@@ -143,7 +201,8 @@ public class DiggingContraints {
 	 */
 	public boolean fireShovel(final Player player, final int level,
 			final BlockBreakEvent event) {
-		if (level < plugin.getConfig().getInteger("Settings.FireShovelLevel")
+		if (level < plugin.getConfig().getInteger(
+				DiggingConst.FIRE_SHOVEL_LEVEL)
 				|| player.getItemInHand().getType() != Material.GOLD_SPADE) {
 			// requirements not met
 			return false;
