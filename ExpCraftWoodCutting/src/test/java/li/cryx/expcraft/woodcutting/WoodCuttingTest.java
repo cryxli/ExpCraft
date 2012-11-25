afa8219e25e46a8606f9ae4bc9ceb2d62dcba92d
cryxli <git@cryx.li>
2012-11-25 10:31:07 +0100

    Completed documentation of wood cutting module

--- ExpCraftWoodCutting/src/test/java/li/cryx/expcraft/woodcutting/WoodCuttingTest.java
+++ ExpCraftWoodCutting/src/test/java/li/cryx/expcraft/woodcutting/WoodCuttingTest.java
@@ -1,3 +1,25 @@
+/*
+ * Copyright (c) 2011 Urs P. Stettler, https://github.com/cryxli
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
 package li.cryx.expcraft.woodcutting;
 
 import org.bukkit.Material;
@@ -8,10 +30,16 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.mockito.Mockito;
 
+/**
+ * Test that ensures the double drops work as expected.
+ * 
+ * @author cryxli
+ */
 public class WoodCuttingTest {
 
 	private static final WoodCutting module = new WoodCutting();
 
+	// test double drops depending on tool quality
 	@Test
 	public void calculateDrop() {
 		Assert.assertNull(testCalcDrop(Material.STONE, Material.STONE));
@@ -22,6 +50,7 @@ public class WoodCuttingTest {
 		testCalcDrop(Material.GOLD_AXE, Material.LOG, 1);
 		testCalcDrop(Material.IRON_AXE, Material.LOG, 2);
 		testCalcDrop(Material.DIAMOND_AXE, Material.LOG, 2);
+
 		Assert.assertNull(testCalcDrop(Material.WOOD_AXE, Material.WOOD));
 		testCalcDrop(Material.STONE_AXE, Material.WOOD, 1);
 		testCalcDrop(Material.GOLD_AXE, Material.WOOD, 1);
@@ -29,6 +58,7 @@ public class WoodCuttingTest {
 		testCalcDrop(Material.DIAMOND_AXE, Material.WOOD, 2);
 	}
 
+	// prepare and test
 	private ItemStack testCalcDrop(final Material itemInHand,
 			final Material block) {
 		Player player = Mockito.mock(Player.class);
@@ -41,6 +71,7 @@ public class WoodCuttingTest {
 		return module.calculateDrop(b, player);
 	}
 
+	// test and verification
 	private void testCalcDrop(final Material itemInHand, final Material block,
 			final int amount) {
 		ItemStack stack = testCalcDrop(itemInHand, block);
